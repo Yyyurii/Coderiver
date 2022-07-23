2052ab3392269bca3442682c82bd36008fb8f773
Yyyurii <yurii.kryvko@gmail.com>
2022-07-23 21:23:29 +0300

    Connected the font

---
+++ src/services/users.js
@@ -0,0 +1,17 @@
+const useUsers = () => {
+  const getUsers = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+    if (!response.ok) {
+      throw new Error(`Could not fetch, status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  return { getUsers };
+};
+
+export default useUsers;
