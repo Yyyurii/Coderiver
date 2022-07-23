a429a6065e50cb90862ead205a7f9f4b83ade54d
Yyyurii <yurii.kryvko@gmail.com>
2022-07-23 22:45:35 +0300

    Written styles for the sidebar

--- src/components/SidebarList/SidebarList.js
+++ src/components/SidebarList/SidebarList.js
@@ -1,13 +1,71 @@
 import "./SidebarList.scss";
 import SidebarListItem from "../SidebarListItem";
+import overview from "../../assets/images/overview.svg";
+import users from "../../assets/images/users.svg";
+import ideas from "../../assets/images/ideas.svg";
+import contacts from "../../assets/images/contacts.svg";
+import agents from "../../assets/images/agents.svg";
+import articles from "../../assets/images/articles.svg";
+import settings from "../../assets/images/settings.svg";
+import subscription from "../../assets/images/subscription.svg";
 
 const SidebarList = () => {
+  const topListItems = [
+    {
+      img: overview,
+      title: "Overview",
+    },
+    {
+      img: users,
+      title: "Users",
+      class: "active",
+    },
+    {
+      img: ideas,
+      title: "Ideas",
+    },
+    {
+      img: contacts,
+      title: "Contacts",
+    },
+    {
+      img: agents,
+      title: "Agents",
+    },
+    {
+      img: articles,
+      title: "Articles",
+    },
+  ];
+
+  const bottomListItems = [
+    {
+      img: settings,
+      title: "Settings",
+    },
+    {
+      img: subscription,
+      title: "Subscription",
+    },
+  ];
+
   return (
-    <ul className="sidebar-list">
-      <SidebarListItem />
-      <SidebarListItem />
-      <SidebarListItem />
-    </ul>
+    <div className="sidebar-list">
+      <div className="sidebar-list__top">
+        <ul>
+          {topListItems.map((item) => (
+            <SidebarListItem key={item.title} details={item} />
+          ))}
+        </ul>
+      </div>
+      <div className="sidebar-list__bottom">
+        <ul>
+          {bottomListItems.map((item) => (
+            <SidebarListItem key={item.title} details={item} />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- src/components/SidebarListItem/SidebarListItem.js
+++ src/components/SidebarListItem/SidebarListItem.js
@@ -1,11 +1,10 @@
 import './SidebarListItem.scss';
-import overveiw from '../../assets/images/overveiw.svg';
 
-const SidebarListItem = () => {
+const SidebarListItem = ({details}) => {
   return (
-    <li className="sidebar-list-item item">
-      <img className="item__img" src={overveiw} alt="icon" />
-      <span className="item__title">Overview</span>
+    <li className={details.class ? "sidebar-list-item item active" : "sidebar-list-item item"}>
+      <img className="item__img" src={details.img} alt="icon" />
+      <span className="item__title">{details.title}</span>
     </li>
   );
 };
