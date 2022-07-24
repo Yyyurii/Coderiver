6396614711843fe1a1d8ef336cbd2c53ffd23d78
Yyyurii <yurii.kryvko@gmail.com>
2022-07-24 18:21:05 +0300

    Created MainTable

--- src/components/MainTable/MainTable.js
+++ src/components/MainTable/MainTable.js
@@ -1,31 +1,50 @@
-import './MainTable.scss';
-import MainTableRow from '../MainTableRow';
-import useUsers from '../../services/users';
-import { useState, useEffect } from 'react';
+import "./MainTable.scss";
+import MainTableRow from "../MainTableRow";
+import useUsers from "../../services/users";
+import { useState, useEffect } from "react";
 
 const MainTable = () => {
-  
+  const [users, setUsers] = useState([]);
   const { getUsers } = useUsers();
 
-  return (
-    <div className="main-table table">
-      <div className="table__header header">
-        <div className="header__item">
-          <span>User details</span>
-        </div>
-        <div className="header__item">
-          <span>Company name</span>
-        </div>
-        <div className="header__item">
-          <span>Email</span>
-        </div>
-        <div className="header__item">
-          <span>Distance</span>
-        </div>
-      </div>
+  useEffect(() => {
+    getUsersList();
+  }, []);
 
-      <MainTableRow />
-    </div>
+  function getUsersList() {
+    getUsers().then((res) => setUsers(res));
+  }
+
+  function renderUsersList(users) {
+    const usersList = users.map((item) => {
+      return <MainTableRow key={item.id} details={item} />;
+    });
+    return usersList;
+  }
+
+  const content = renderUsersList(users);
+
+  return (
+    <table className="main-table table">
+      <thead>
+        <tr className="table__header header">
+          <td className="header__item">
+            <span>User details</span>
+          </td>
+          <td className="header__item">
+            <span>Company name</span>
+          </td>
+          <td className="header__item">
+            <span>Email</span>
+          </td>
+          <td className="header__item">
+            <span>Distance</span>
+          </td>
+        </tr>
+      </thead>
+
+      <tbody>{content}</tbody>
+    </table>
   );
 };
 
